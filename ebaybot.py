7f2db8131f6e3c7cc7ec88b41352c952198afd6c
Howl Jenkins Pendragon <adrainogomis61@gmail.com>
2017-08-04 02:45:30 +0000

    3

--- ebaybot.py
+++ ebaybot.py
@@ -3,49 +3,48 @@ from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 import json
 import datetime
+import threading
 
-respuesta = ''
+respuesta = []
 def buscarebay(busqueda):
-	try:
-		api = Finding(appid="YOUR EBAY APP ID", config_file=None)
-		response = api.execute('findItemsAdvanced', {'keywords': busqueda)
+		api = Finding(appid="HowlJenk-WaLSearc-PRD-c6c5cebfd-8b15464c", config_file=None)
+		response = api.execute('findItemsAdvanced', {'keywords': busqueda})
 		try:
-				if response.reply.searchResult.item != '':
-					for a in response.reply.searchResult.item:
-						global respuesta
-						precio = "El precio es: " + a.sellingStatus.currentPrice.value
-						Titulo = "Producto: " + a.title
-						metodo_pago = "Pago con: " + str(a.paymentMethod)
-						url = "URL: " + a.viewItemURL
-						respuesta = Titulo + "\n" + precio + "\n" + metodo_pago + "\n" + url
-				else:
-					busqueda = "Consolador"
-					response = api.execute('findItemsAdvanced', {'keywords': busqueda})
-					for a in response.reply.searchResult.item:
-						precio = "El precio es: " + a.sellingStatus.currentPrice.value
-						Titulo = "Producto: " + a.title
-						metodo_pago = "Pago con: " + str(a.paymentMethod)
-						url = "URL: " + a.viewItemURsL
-						respuesta = Titulo + "\n" + precio + "\n" + metodo_pago + "\n" + url
-		except Exception as e:
-			raise
+			if response.reply.searchResult.item != '':
+				for a in response.reply.searchResult.item:
+					global respuesta
+					precio = "El precio es: " + a.sellingStatus.currentPrice.value
+					Titulo = "Producto: " + a.title
+					metodo_pago = "Pago con: " + str(a.paymentMethod)
+					url = "URL: " + a.viewItemURL
+					respuesta.append(Titulo + "\n" + precio + "\n" + metodo_pago + "\n" + url)
+			else:
+				pass
 
+		except ConnectionError as e:
+			print(e)
+			print(e.response.dict())
 
-	except ConnectionError as e:
-		print(e)
-		print(e.response.dict())
-
-bot = telebot.TeleBot("Your token")
+bot = telebot.TeleBot("423141802:AAE7Cd8T0a0RtiJroBBsazr7Aij-xUUuHY4")
 @bot.message_handler(commands=['buscar'])
+def buscarfalso(message):
+	threading.Thread(target=buscar, args=(message,)).start()
 def buscar(message):
+	global respuesta
 	cid = message.chat.id
 	if message.content_type == 'text':
 		if len(message.text.split('/buscar ')) == 2:
 			mclear = message.text.split('/buscar ')[1]
 			buscarebay(mclear)
-			bot.send_chat_action(cid, 'typing')
 			print("[" + str(datetime.datetime.now()) + "] " + str(message.chat.first_name) + " [" + str(message.chat.id) + "]: " + message.text)
-			bot.reply_to(message, respuesta)
+			bot.send_chat_action(cid, 'typing')
+			while True:
+				for a in respuesta:
+					bot.reply_to(message,a)
+
+				respuesta = []
+
+
 		else:
 			bot.reply_to(message, "Que quieres buscar?")
 			pass
@@ -53,5 +52,6 @@ def buscar(message):
 def help(message):
 	cid = message.chat.id
 	bot.reply_to(message, "Bienvenido al bot de busqueda en ebay, creado por @etarra(Howl)  \n \n/buscar + Articulo para buscar un producto en ebay con el menor precio")
+
 
 bot.polling()
